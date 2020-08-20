425a09dc9de8b8ef69fb2481a1752536715da9e3
Your Name <you@example.com>
2020-08-20 14:11:25 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <p>peter m</p>
+      <p>Lorem </p>
+      <p>Lorem </p> <p>Lorem </p> <p>Lorem </p> <p>Lorem </p> <p>Lorem </p>{" "}
+      <p>Lorem </p> <p>Lorem </p> <p>Lorem </p> <p>Lorem </p> <p>Lorem </p>{" "}
+      <p>Lorem </p> <p>Lorem </p> <p>Lorem </p>
     </div>
   );
 }
