bf421ed6010eb6521a0c42eb178b45f1daf17193
Your Name <you@example.com>
2020-08-20 23:09:10 +0300

    md

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,24 +2,32 @@ import React from "react";
 import "./home.css";
 import Typed from "react-typed";
 import mgikunda from "./mgikunda.jpg";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 const home = () => {
   return (
-    <div className="hero">
-      <img src={mgikunda} alt="the devs pic" className="pic" />
-      <div className="hero-text">
-        <h1>I am Mgikunda</h1>
-        <Typed
-          strings={[
-            "I am a fullstack Web Developer",
-            "I am an IOT enthusiast",
-            "I enjoy writing code",
-          ]}
-          typeSpeed={40}
-          backSpeed={50}
-          loop
-        />
-      </div>
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <div className="hero">
+          <img src={mgikunda} alt="the devs pic" className="pic" />
+          <div className="hero-text">
+            <h1>I am Mgikunda</h1>
+            <Typed
+              strings={[
+                "I am a fullstack Web Developer",
+                "I am an IOT enthusiast",
+                "I enjoy writing code",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
