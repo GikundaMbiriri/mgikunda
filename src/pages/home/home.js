4bf8032f8f6b9f57eabc101effb3d3b1535e9c9c
Your Name <you@example.com>
2020-08-21 01:00:24 +0300

    md

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,30 +10,21 @@ const home = () => {
     <>
       <CssBaseline />
       <Container>
-        <div class="sidebar">
-          <a class="active" href="#home">
-            Home
-          </a>
-          <a href="#about">About</a>
-          <a href="#projects">projects</a>
-          <a href="#contact">Contact</a>
-        </div>
-        <div className="content">
-          <div className="hero" id="home">
-            <img src={mgikunda} alt="the devs pic" className="pic" />
-            <div className="hero-text">
-              <h1>I am Mgikunda</h1>
-              <Typed
-                strings={[
-                  "I am a fullstack Web Developer",
-                  "I am an IOT enthusiast",
-                  "I enjoy writing code",
-                ]}
-                typeSpeed={40}
-                backSpeed={50}
-                loop
-              />
-            </div>
+        <div className="hero" id="home">
+          <img src={mgikunda} alt="the devs pic" className="pic" />
+          <div className="hero-text">
+            <h1>I am Mgikunda</h1>
+            <Typed
+              strings={[
+                "I am a fullstack Web Developer",
+                "I am an IOT enthusiast",
+                "I enjoy writing code",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+
             <hr />
           </div>
           <div></div>
